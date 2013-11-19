2661deccc344218de881df32b062c2ea59c94045
cavanagh <cavanagh@dsv.su.se>
2013-11-19 10:58:42 +0100

    dsv_theme

---
+++ themes/dsv_theme/region--footer.tpl.php
@@ -0,0 +1,20 @@
+<!-- region--footer.tpl.php -->
+
+<div class="footer-profile">
+  <div class="profile-info" style="display: block;">
+    <br />
+  </div>
+</div>
+
+<div class="footer-top"></div>
+
+<div class="clear-float"></div>
+
+<div class="footer-left">
+  <address>
+    <b>Institutionen f&ouml;r data- och systemvetenskap, DSV</b><br/>
+      Stockholms universitet, DSV, Forum 100, 164 40 Kista | Telefon: <a href="tel:08162000">08-16 20 00</a>
+  </address>
+</div>
+
+<div class="clear-float"></div>
